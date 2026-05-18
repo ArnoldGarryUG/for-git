27f74e12e50880e68e635d7db936e969e92c85dd
ArnoldGarryUG <garryarnold70@gmail.com>
2026-05-18 20:52:30 +0300

    Update whgg.js

--- js/whgg.js
+++ js/whgg.js
@@ -1,1 +1,3 @@
 console.log('whgg.js loaded');
+
+<script type="text/javascript">!function(){ var e=document.createElement("script");e.type="text/javascript",e.async=!0;var t="https://statics2.kudobuzz.com/widget-loader.min.js";e.src=t;document.getElementsByTagName("head")[0].appendChild(e);window.Kudos={ServerName: "https://statics2.kudobuzz.com/", aid: '699f095c9b0777307b49399c', hideNeptune: true, apiKey: '594a28ac7dc86ea1240e0f43', WidgetServer: "https://widget.kudobuzz.com/", apiServer:"https://api.kudobuzz.com" }}()</script><div data-kb-widget-type="kb-neptune-widget"></div>
